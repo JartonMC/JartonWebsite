229feb9c3e99629cb177ac0548261ad1a8298529
Huzaifa Mughal <92018968+HuzaifaMughal0024@users.noreply.github.com>
2025-12-10 22:37:00 +0500

    fixed some stuff

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-import createMDX from "@next/mdx";
-import type { NextConfig } from "next";
-
-const withMDX = createMDX({ extension: /\.(md|mdx)$/ });
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  /* config options here */
+}
 
-  // Force Webpack instead of Turbopack
-  webpack: (config) => {
-    return config; // simply use Webpack, do not modify Turbopack
-  },
-};
-
-export default withMDX(nextConfig);
+export default nextConfig
